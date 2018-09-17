0f67b6079dfef7dea7b7dc4cd50de266e8eb8a00
Wei Yuhan <weiyuhan@pku.edu.cn>
2018-09-17 13:50:37 +0800

    save actions

--- models/action.js
+++ models/action.js
@@ -1,16 +1,10 @@
 const mongoose=require("mongoose");
 
 var ActionSchema=new mongoose.Schema({
-        round_id: { type: Number,required: true, index:true },  
-        action_id: { type: Number,required: true, index:true },
-        time_stamp: { type: String }, // formatted time, e.g. 2017-10-31 14:00:20
+        round_id: { type: Number, required: true, index:true },  
+        time: { type: Number,required: true, index:true }, // formatted time, e.g. 2017-10-31 14:00:20
         player_name: { type: String }, // playername
-        // by_system: { type: Boolean }, // if assisted by system recommendation
-        operation: { type: String }, // [++/+/--/-]
-        from: { type: String },
-        direction: {type: String },
-        to: { type: String },
-        contribution: { type: Number } // the player's contribution of this action
+        links_size: { type: Object}, // [{x, y, tag, size, behinted}]
 });
 
 console.log('[OK] Action Schema Created.');
