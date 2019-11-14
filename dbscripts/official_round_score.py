ee36f4cf5492130c1e29470ed50914990d34673a
weiyuhan <weiyuhan@pku.edu.cn>
2019-11-14 13:24:24 +0800

    remove resetplace

--- dbscripts/official_round_score.py
+++ dbscripts/official_round_score.py
@@ -25,8 +25,34 @@ for record in official_records:
 		user_map[record['username']]['score'] += record['score'] if record['score'] > 0 else 0
 		user_map[record['username']]['round'].add(record['round_id'])
 
+
 print(user_map)
 for username, data in user_map.items():
 	db['users'].update_one(
 		{'username': username}, 
 		{'$set': {'round_attend': len(data['round']), 'total_score': data['score']}})
+
+
+class_map = defaultdict(lambda: {
+	'players': set(),
+	'score': 0})
+for username, data in user_map.items():
+	if username[0] != '1' or len(username) != 10 or data['score'] <= 0:
+		continue
+	class_map[username[:8]]['score'] += data['score']
+	class_map[username[:8]]['players'].add(username)
+print(class_map)
+with open('classscore.csv', 'w+') as f:
+	f.write('class_id,attended_players,total_score\n')
+	for class_id, data in class_map.items():
+		f.write('%s,%s,%s\n' % (class_id, str(len(data['players'])), str(data['score'])))
+
+
+
+with open('userscore.csv', 'w+') as f:
+	f.write('username,round_attended,total_score\n')
+	for username, data in user_map.items():
+		round_attended, total_score = len(data['round']), data['score']
+		f.write('%s,%s,%s\n' % (username, str(round_attended), str(total_score)))
+
+
